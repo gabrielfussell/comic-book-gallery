2633a81fee992ed49e00643c26f17c100f5ffb2d
Gabe Fussell <gabrielfussell@gmail.com>
2022-02-24 19:25:45 -0500

    Removed the comic book index view and action method. Updated the home index view to show a list of all comic books.

--- src/ComicBookGallery/ComicBookGallery/Controllers/HomeController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 ﻿using ComicBookGallery.Models;
+using ComicBookGallery.Data;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+
 
 namespace ComicBookGallery.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ComicBookRepository _comicBookRepository;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _comicBookRepository = new ComicBookRepository();
         }
 
         public IActionResult Index()
         {
-            return View();
+            ComicBook[] comicBooks = _comicBookRepository.GetComicBooks();
+            return View(comicBooks);
         }
 
         public IActionResult Privacy()
